a028573e23133d8c40f10762a1c50f010d45c2e4
Edgar <epineda0313@gmail.com>
2020-11-20 13:11:02 -0500

    react setup

---
+++ client/src/components/tweets/tweets.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class tweets extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default tweets
