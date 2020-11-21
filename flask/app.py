27e2bbb7bda3165b5e3b363b668b07047fd0a0b2
Caitlin Sanders <cmsander@syr.edu>
2020-11-21 12:46:12 -0500

    Code clean up

--- flask/app.py
+++ flask/app.py
@@ -3,7 +3,6 @@
 #----------------------------------------------------------------------------#
 
 from flask import Flask, render_template, request
-# from flask.ext.sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from forms import *
@@ -14,148 +13,20 @@ from ibm_watson import PersonalityInsightsV3
 import json
 from os.path import join
 from flask import jsonify
+from results import get_results
+import sys
+from flask_cors import CORS
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
 
 app = Flask(__name__)
 app.config.from_object('config')
-#db = SQLAlchemy(app)
-
-# Automatically tear down SQLAlchemy.
-'''
-@app.teardown_request
-def shutdown_session(exception=None):
-    db_session.remove()
-'''
-
-# Login required decorator.
-'''
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('login'))
-    return wrap
-'''
+CORS(app)
 #----------------------------------------------------------------------------#
 # Controllers.
 #----------------------------------------------------------------------------#
 
-
-@app.route('/')
-def home():
-    return render_template('pages/placeholder.home.html')
-
-
-@app.route('/about')
-def about():
-    return render_template('pages/placeholder.about.html')
-
-
-@app.route('/login')
-def login():
-    form = LoginForm(request.form)
-    return render_template('forms/login.html', form=form)
-
-
-@app.route('/register')
-def register():
-    form = RegisterForm(request.form)
-    return render_template('forms/register.html', form=form)
-
-
-@app.route('/forgot')
-def forgot():
-    form = ForgotForm(request.form)
-    return render_template('forms/forgot.html', form=form)
-
-@app.route('/insights/')
-def insights():
-    auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
-    auth.set_access_token(config.access_token, config.access_token_secret)
-    api = tweepy.API(auth)
-
-    personality_insights = PersonalityInsightsV3(
-        version=config.myVersion,
-        iam_apikey=config.myIam_apikey,
-        url=config.myUrl
-    )
-
-    personality = {
-        'adventurous' : 0,
-        'emotionally-intelligent' : 0,
-        'imaginative' : 0,
-        'intellectual' : 0,
-
-        'cautious' : 0,
-        'orderly' : 0,
-        'disciplined' : 0,
-
-        'assertive' : 0,
-        'cheerful' : 0,
-        'outgoing' : 0,
-
-        'modest' : 0,
-        'uncompromising' : 0,
-        'sympathetic' : 0,
-        'trusting' : 0,
-
-        'worry-prone' : 0,
-        'melancholic' : 0,
-        'self-conscious' : 0,
-        'stress-prone' : 0,
-    }
-
-
-    user = request.args.get('q')
-    public_tweets = api.user_timeline(user)
-    newTweets = []
-    for tweet in public_tweets:
-        newTweets.append(tweet.text)
-    tweets= {'contentItems': newTweets}
-
-    with open('./profile.json', 'w') as fp:
-        json.dump(tweets, fp, indent=2)
-
-    with open('./profile.json') as profile_json:
-        profile = personality_insights.profile(
-            profile_json.read(),
-            'application/json',
-            content_type='application/json',
-            consumption_preferences=True,
-            raw_scores=True
-        ).get_result()
-
-    personality['adventurous'] += profile['personality'][0]['children'][0]['raw_score']
-    personality['emotionally-intelligent'] += profile['personality'][0]['children'][2]['raw_score']
-    personality['imaginative'] += profile['personality'][0]['children'][3]['raw_score']
-    personality['intellectual'] += profile['personality'][0]['children'][4]['raw_score']
-
-    personality['cautious'] += profile['personality'][1]['children'][1]['raw_score']
-    personality['orderly'] += profile['personality'][1]['children'][3]['raw_score']
-    personality['disciplined'] += profile['personality'][1]['children'][4]['raw_score']
-
-    personality['assertive'] += profile['personality'][2]['children'][1]['raw_score']
-    personality['cheerful'] += profile['personality'][2]['children'][2]['raw_score']
-    personality['outgoing'] += profile['personality'][2]['children'][4]['raw_score']
-
-    personality['modest'] += profile['personality'][3]['children'][2]['raw_score']
-    personality['uncompromising'] += profile['personality'][3]['children'][3]['raw_score']
-    personality['sympathetic'] += profile['personality'][3]['children'][4]['raw_score']
-    personality['trusting'] += profile['personality'][3]['children'][5]['raw_score']
-
-    personality['worry-prone'] += profile['personality'][4]['children'][0]['raw_score']
-    personality['melancholic'] += profile['personality'][4]['children'][0]['raw_score']
-    personality['self-conscious'] += profile['personality'][4]['children'][0]['raw_score']
-    personality['stress-prone'] += profile['personality'][4]['children'][0]['raw_score']
-    
-    personalities = sorted(personality, key=personality.get, reverse=True)[:3]
-    print(sorted(personality, key=personality.get, reverse=True))
-    return render_template('pages/personality-insights.html', tweets=public_tweets, personalities=personalities)
 
 @app.route('/view/')
 def view():
@@ -165,14 +36,13 @@ def view():
 
     try:
         user = request.args.get('q')
-        public_tweets = api.user_timeline(user)
+        return_json = jsonify(get_results(api, user))
     except:
-        return(jsonify({"Error": "Error"}))
-    return(jsonify({"Success": "Sucecss"}))
+        return(jsonify({'status': 0})) # 0 error, 1 passes
+
+    return(return_json)
     
 # Error handlers.
-
-
 @app.errorhandler(500)
 def internal_error(error):
     #db_session.rollback()
---
+++ flask/results.py
@@ -0,0 +1,97 @@
+import tweepy
+import config
+from ibm_watson import PersonalityInsightsV3
+import json
+from os.path import join
+from flask import jsonify
+
+def get_results(api, user):
+
+    return_json = {'status':1}
+    public_tweets = api.user_timeline(user)
+
+    impression = get_impression(public_tweets)
+
+    return_json['first_impression'] = impression
+
+    return(return_json)
+
+
+
+def get_impression(public_tweets):
+
+    personality_insights = PersonalityInsightsV3(
+        version=config.myVersion,
+        iam_apikey=config.myIam_apikey,
+        url=config.myUrl
+    )
+
+    personality = {
+        'adventurous' : 0,
+        'emotionally-intelligent' : 0,
+        'imaginative' : 0,
+        'intellectual' : 0,
+
+        'cautious' : 0,
+        'orderly' : 0,
+        'disciplined' : 0,
+
+        'assertive' : 0,
+        'cheerful' : 0,
+        'outgoing' : 0,
+
+        'modest' : 0,
+        'uncompromising' : 0,
+        'sympathetic' : 0,
+        'trusting' : 0,
+
+        'worry-prone' : 0,
+        'melancholic' : 0,
+        'self-conscious' : 0,
+        'stress-prone' : 0,
+    }
+
+
+    newTweets = []
+    for tweet in public_tweets:
+        newTweets.append(tweet.text)
+    tweets= {'contentItems': newTweets}
+
+    with open('./profile.json', 'w') as fp:
+        json.dump(tweets, fp, indent=2)
+
+    with open('./profile.json') as profile_json:
+        profile = personality_insights.profile(
+            profile_json.read(),
+            'application/json',
+            content_type='application/json',
+            consumption_preferences=True,
+            raw_scores=True
+        ).get_result()
+
+    personality['adventurous'] += profile['personality'][0]['children'][0]['raw_score']
+    personality['emotionally-intelligent'] += profile['personality'][0]['children'][2]['raw_score']
+    personality['imaginative'] += profile['personality'][0]['children'][3]['raw_score']
+    personality['intellectual'] += profile['personality'][0]['children'][4]['raw_score']
+
+    personality['cautious'] += profile['personality'][1]['children'][1]['raw_score']
+    personality['orderly'] += profile['personality'][1]['children'][3]['raw_score']
+    personality['disciplined'] += profile['personality'][1]['children'][4]['raw_score']
+
+    personality['assertive'] += profile['personality'][2]['children'][1]['raw_score']
+    personality['cheerful'] += profile['personality'][2]['children'][2]['raw_score']
+    personality['outgoing'] += profile['personality'][2]['children'][4]['raw_score']
+
+    personality['modest'] += profile['personality'][3]['children'][2]['raw_score']
+    personality['uncompromising'] += profile['personality'][3]['children'][3]['raw_score']
+    personality['sympathetic'] += profile['personality'][3]['children'][4]['raw_score']
+    personality['trusting'] += profile['personality'][3]['children'][5]['raw_score']
+
+    personality['worry-prone'] += profile['personality'][4]['children'][0]['raw_score']
+    personality['melancholic'] += profile['personality'][4]['children'][0]['raw_score']
+    personality['self-conscious'] += profile['personality'][4]['children'][0]['raw_score']
+    personality['stress-prone'] += profile['personality'][4]['children'][0]['raw_score']
+    
+    personalities = sorted(personality, key=personality.get, reverse=True)[:3]
+
+    return(personalities)
